1d9bc280dc876b70900f340774398c361cfdfcf9
Arkhrer <jonasrngl@gmail.com>
2024-07-03 12:28:39 -0300

    Bugfixing and version setting and minor improvements

--- Globals.py
+++ Globals.py
@@ -17,7 +17,7 @@ def initialize():
     dockers = {}
     availablePorts = []
     currentPort = 0
-    numberOfThreads = 100
+    numberOfThreads = 16
     for i in range(numberOfThreads * 2):
         availablePorts.append(8000 + i)
 
